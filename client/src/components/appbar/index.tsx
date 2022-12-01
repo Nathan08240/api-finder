974126e73916d413bede7c43c12e225b291700c4
florianln <florian.laurain@gmail.com>
2022-12-01 13:59:25 +0100

    add searchbar

--- client/src/components/appbar/index.tsx
+++ client/src/components/appbar/index.tsx
@@ -1,8 +1,60 @@
-import {AppBar, Toolbar, Typography, Button} from "@mui/material";
-import {Close, Delete, FileDownload, FileUpload, Menu, MoreVert, Update} from "@mui/icons-material";
-import {Sidebar} from "../sidebar";
-import {useState} from "react";
+import React from "react";
+import { AppBar, Toolbar, Typography, Button } from "@mui/material";
+import { Close, Delete, FileDownload, FileUpload, Menu, MoreVert, Update } from "@mui/icons-material";
+import SearchIcon from '@mui/icons-material/Search';
+import { Sidebar } from "../sidebar";
+import { useState } from "react";
 import { Outlet } from "react-router-dom";
+import { styled, alpha } from "@mui/material/styles";
+import InputBase from "@mui/material/InputBase";
+
+const Search = styled('div')(({ theme }) => ({
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+        backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+        marginLeft: theme.spacing(3),
+        width: 'auto',
+    },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: 'inherit',
+    '& .MuiInputBase-input': {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('md')]: {
+            width: '20ch',
+        },
+    },
+}));
+
+// export default function PrimarySearchAppBar() {
+//     const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+//     const [mobileMoreAnchorEl, setMobileMoreAnchorEl] =
+//         React.useState<null | HTMLElement>(null);
+// }
+
+
 export const AppBarHead = () => {
 
     const [open, setOpen] = useState(false);
@@ -15,19 +67,29 @@ export const AppBarHead = () => {
         setOpen(false);
     }
 
+
     return (
         <>
             <AppBar position="absolute">
                 <Toolbar>
-                    <Typography variant="h6" component="div" sx={{flexGrow: 1, textAlign: "center"}}>
-                        
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1, textAlign: "center" }}>
+
                     </Typography>
+                    <Search>
+                        <SearchIconWrapper>
+                            <SearchIcon />
+                        </SearchIconWrapper>
+                        <StyledInputBase
+                            placeholder="Search…"
+                            inputProps={{ 'aria-label': 'search' }}
+                        />
+                    </Search>
                     <Button
-                        sx={{position: "absolute", left: 0}}
+                        sx={{ position: "absolute", left: 0 }}
                         color="inherit"
                         onClick={open ? handleDrawerClose : handleDrawerOpen}
                     >
-                        {open ? <Close sx={{color: "black", zIndex: 999 }}/> : <Menu/>}
+                        {open ? <Close sx={{ color: "black", zIndex: 999 }} /> : <Menu />}
                     </Button>
                     <Button
                         color="inherit"
@@ -70,10 +132,10 @@ export const AppBarHead = () => {
                         <MoreVert />
                     </Button>
                 </Toolbar>
-                <Sidebar open={open}/>
+                <Sidebar open={open} />
             </AppBar>
             <div id="detail">
-                <Outlet/>
+                <Outlet />
             </div>
         </>
     );
