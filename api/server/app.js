e410d1c057fa289f1da87fc7ff83dc9b47c9fe43
jordan <jbs.io@protonmail.com>
2022-12-01 16:02:44 +0100

    💄 Login

--- api/server/app.js
+++ api/server/app.js
@@ -1,30 +1,27 @@
-require("./db/mongoose");
-require("./utils/redis");
-const express = require("express");
-const path = require("path");
-const logger = require("morgan");
-const initRouter = require("./routes/index");
-const cors = require("cors");
+require('./db/mongoose')
+require('./utils/redis')
+const express = require('express')
+const path = require('path')
+const logger = require('morgan')
+const initRouter = require('./routes/index')
+const cors = require('cors')
 
-const app = express();
+const app = express()
 
-const allowedOrigins = [
-    "http://localhost:5173",
-    ];
+const allowedOrigins = ['http://localhost:5173']
 
+app.use(
+  cors({
+    origin: allowedOrigins,
+  })
+)
 
+app.use(express.json())
+app.use(logger('dev'))
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use(cors({
-    origin: allowedOrigins
-}));
-
-
-app.use(express.json());
-app.use(logger("dev"));
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
-
-initRouter(app);
+initRouter(app)
 
 console.log(`
 
@@ -35,6 +32,6 @@ console.log(`
 ██      ██ ██   ████ ██████  ███████ ██   ██                                            
                                             
 App running on http://localhost:${process.env.PORT}
-`);
+`)
 
-module.exports = app;
+module.exports = app
